b068deb2415994a75d00a385ab3a4c79aac490c9
Adamsx320 <166951770+Adamsx320@users.noreply.github.com>
2024-07-01 03:10:20 +0000

    Integrantes

---
+++ Integrantes.py
@@ -0,0 +1,11 @@
+# integrantes.py
+
+# Lista de nombres y apellidos de los integrantes del grupo
+integrantes = [
+    "Katherine Gonzalez",
+    "Adams Duran"
+]
+
+# Imprimir los nombres y apellidos
+for integrante in integrantes:
+    print(integrante)
